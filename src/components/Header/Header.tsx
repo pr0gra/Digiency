9bdb79d7823a9db06dffbb01e3ec000fa7a748af
pr0gra <denisovProg@gmail.com>
2022-11-03 18:19:31 +0500

    comment

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -11,8 +11,42 @@ export function Header() {
         src={digiencyMainLogo}
         alt=""
       />
-      <img className={styles['theme-icon']} src={themeIcon} alt="" />
       <img className={styles['burger-menu']} src={burgerMenu} alt="" />
+      <nav className={styles['navigation']}>
+        <ul className={styles['list']}>
+          <li className={styles['item']}>
+            <a className={styles['item-link']}>
+              <h4 className={styles['item-text']}>Home</h4>
+            </a>
+          </li>
+          <li className={styles['item']}>
+            <a className={styles['item-link']}>
+              <h4 className={styles['item-text']}>About Us</h4>
+            </a>
+          </li>
+          <li className={styles['item']}>
+            <a className={styles['item-link']}>
+              <h4 className={styles['item-text']}>Our Work</h4>
+            </a>
+          </li>
+          <li className={styles['item']}>
+            <a className={styles['item-link']}>
+              <h4 className={styles['item-text']}>Clients</h4>
+            </a>
+          </li>
+          <li className={styles['item']}>
+            <a className={styles['item-link']}>
+              <h4 className={styles['item-text']}>Our Blog</h4>
+            </a>
+          </li>
+          <li className={styles['item']}>
+            <a className={styles['item-link']}>
+              <h4 className={styles['item-text']}>Contact Us</h4>
+            </a>
+          </li>
+        </ul>
+      </nav>
+      <img className={styles['theme-icon']} src={themeIcon} alt="" />
     </div>
   )
 }
